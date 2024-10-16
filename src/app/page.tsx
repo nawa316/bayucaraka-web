371c6fd7891dde322bb2503978e073240f870525
nawa316 <muhammadadedzakwan@gmail.com>
2024-10-16 07:18:41 +0700

    feat: update content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Masih ETS Huhuhuhuuu</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
